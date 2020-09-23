145290f9fb2e4affc4197d23098325c8a497de52
George Kochera <george.kochera@gmail.com>
2020-09-22 20:49:46 -0400

    sql query working need to create template

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 from flask import Flask, render_template, url_for, redirect, request, make_response, jsonify
-from flask_mysqldb import MySQL
+from sqlalchemy import MetaData, Table, Column, Integer, String, Sequence, create_engine, select, text, exc
 import yaml
 
+# Create the Flask App
 app = Flask(__name__)
 
-db = yaml.load(open('config.yaml'))
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
-mysql = MySQL(app)
+# Configure the Database and connect
+db_config = yaml.load(open('config.yaml'))
+db_url = db_config["url"]
+engine = create_engine(db_url, pool_size=20, echo=True)
+metadata = MetaData(bind=engine)
 
 @app.route('/')
 def index():
-    return render_template("content.jinja2", content="<p>TEST</p>")
+    with open("bsg_db.sql", mode="r") as file:
+        query = text(file.read())
+        response_string = ""
+    with engine.connect() as eng:
+        eng.execute(query)
+        metadata.reflect()
+        bsg_people = Table("bsg_people", metadata)
+        select_query = select([bsg_people])
+        result = eng.execute(select_query)
+        for row in result:
+            print(row)
+    
+
+    return render_template("content.jinja2", content=str(response_string))
 
 if __name__ == '__main__':
     app.run(port=5001)
